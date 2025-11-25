8498c799478cd5638d8649b09bbac37ffcd3e616
JacopoHasFunProgramming <160734900+Jacopogaetani@users.noreply.github.com>
2025-11-25 20:09:43 +0100

    Add files via upload

---
+++ Python/quotesStellari.py
@@ -0,0 +1,9 @@
+import random
+
+numero = random.randint(0,4)
+
+
+quotes = ["Odio i bianchi come odio i neri come odio i gialli come amo le gialle", "Se Mohammed Rasul Allah non va dalla montagna la montagna va da Mohammed Rasul Allah","Attacca banda"
+          , "Appust","Non scherzare con noi JJK fans, non abbiamo letto manco il nostro manga",  ]
+
+print(quotes[0])
